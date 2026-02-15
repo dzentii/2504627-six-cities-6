5fb722745343f2043cda1bda03e02c83753d2f03
dzentii <rzhnik@gmail.com>
2026-02-15 18:55:42 +0500

    fix lint

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,6 +1,7 @@
 import { readFileSync } from 'node:fs';
 import { EventEmitter } from 'node:events';
 import { Offer, HousingType, CityName } from '../../../types/offer.type';
+import { UserType } from '../../../types/user.type.js';
 
 export default class TSVFileReader extends EventEmitter {
   constructor(private readonly filename: string) {
@@ -9,7 +10,7 @@ export default class TSVFileReader extends EventEmitter {
 
   public read(): void {
     const rawData = readFileSync(this.filename, { encoding: 'utf-8' });
-    
+
     const lines = rawData
       .split('\n')
       .filter((row) => row.trim().length > 0)
@@ -23,7 +24,7 @@ export default class TSVFileReader extends EventEmitter {
     const [
       title, description, postDate, city, previewImage, images,
       isPremium, isFavorite, rating, type, bedrooms, maxAdults,
-      price, goods, name, email, avatarPath, password, userType, 
+      price, goods, name, email, avatarPath, password, userType,
       latitude, longitude
     ] = line.split('\t');
 
@@ -42,10 +43,16 @@ export default class TSVFileReader extends EventEmitter {
       maxAdults: Number.parseInt(maxAdults, 10),
       price: Number.parseInt(price, 10),
       goods: goods.split(','),
-      author: { name, email, avatarPath, password, type: userType as any },
-      location: { 
-        latitude: Number.parseFloat(latitude), 
-        longitude: Number.parseFloat(longitude) 
+      author: {
+        name,
+        email,
+        avatarPath,
+        password,
+        type: userType as UserType
+      },
+      location: {
+        latitude: Number.parseFloat(latitude),
+        longitude: Number.parseFloat(longitude)
       },
     };
   }
